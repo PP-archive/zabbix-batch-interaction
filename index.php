de26b621c4004543d8594e08ecfe0703b46080b9
Pavel Polyakov <pavel@kredito.de>
2014-11-02 20:53:36 +0200

    sync

--- index.php
+++ index.php
@@ -1,48 +1,71 @@
 <?php
+
 require "vendor/autoload.php";
 
 $f3 = require('vendor/bcosca/fatfree/lib/base.php');
 
-$f3->route('GET /',
-    function($f3) {
-        $zabbixClient = new Zabbix\Client();
-        
-        $f3->set('var', $zabbixClient->hello());
-        
-        $template=new Template();
-        echo $template->render('templates/index.htm');
+$f3->config('application.cfg');
+$config = $f3->get('config');
+
+$f3->route('GET /', function($f3) use ($config) {
+//        $zabbixClient = new Zabbix\Client($config['apiUrl'], $config['user'], $config['password']);
+//        
+//        $zabbixClient->auth();
+//        $zabbixClient->getHosts();
+//        $zabbixClient->getApplicationsByHostId([10207]);
+    //$f3->set('var', $zabbixClient->hello());
+
+    if(is_file('.config')) {
+        $_config = json_decode(file_get_contents('.config'), true);
     }
+    
+    $f3->set('api_url', isset($_config['api_url'])?$_config['api_url']:null);
+    $f3->set('user', isset($_config['user'])?$_config['user']:null);
+    $f3->set('password', isset($_config['password'])?$_config['password']:null);
+
+    $template = new Template();
+    echo $template->render('templates/index.htm');
+}
 );
 
-$f3->route('POST /create',
-    function($f3) {
-        $host = $f3->get('POST.host');
-        $nameTemplate = $f3->get('POST.name_template');
-        $keyTemplate = $f3->get('POST.key_template');
-        $values = $f3->get('POST.values');
-        $type = $f3->get('POST.type');
-        $formula = $f3->get('POST.formula');
-        
-        $values = explode("\n", $values);
-        
-        foreach($values as $value) {
-            $value = trim($value);
-            $value = str_replace(array("."), "", $value);
-            
-            $name = str_replace("*", $value, $nameTemplate);
-            $key = str_replace("*", $value, $keyTemplate);
-            $formula = str_replace("*", $value, $formulaTemplate);
-            
-            var_dump($name, $key, $formula);
-            echo "<hr/>";
-        }
+$f3->route('POST /create', function($f3) {
+    $host = $f3->get('POST.host');
+    $nameTemplate = $f3->get('POST.name_template');
+    $keyTemplate = $f3->get('POST.key_template');
+    $values = $f3->get('POST.values');
+    $type = $f3->get('POST.type');
+    $formula = $f3->get('POST.formula');
+
+    $values = explode("\n", $values);
+
+    foreach ($values as $value) {
+        $value = trim($value);
+        $value = str_replace(array("."), "", $value);
+
+        $name = str_replace("*", $value, $nameTemplate);
+        $key = str_replace("*", $value, $keyTemplate);
+        $formula = str_replace("*", $value, $formulaTemplate);
+
+        var_dump($name, $key, $formula);
+        echo "<hr/>";
     }
+}
 );
 
-$f3->route('POST /remove',
-    function($f3) {
-        echo $f3->get('POST.name_template');
-    }
+$f3->route('POST /remove', function($f3) {
+    echo $f3->get('POST.name_template');
+}
+);
+
+$f3->route('POST /config', function($f3) use ($config) {
+    $_config = ['api_url' => $f3->get('POST.api_url'),
+        'user' => $f3->get('POST.user'),
+        'password' => $f3->get('POST.password')];
+    
+    file_put_contents("./.config", json_encode($_config));
+    
+    Header("Location: ".$config['home']);
+}
 );
 
 $f3->run();
--- src/Zabbix/Client.php
+++ src/Zabbix/Client.php
@@ -4,8 +4,97 @@ namespace Zabbix;
 
 class Client {
 
-    public function hello() {
-        return "hello";
+    protected $_apiUrl = null;
+    protected $_user = null;
+    protected $_password = null;
+    protected $_auth = null;
+    
+    public function __construct($apiUrl, $user, $password) {
+        $this->_apiUrl = $apiUrl;
+        $this->_user = $user;
+        $this->_password = $password;
+    }
+    
+    protected function _request($request) {
+        $request = json_encode($request);
+        
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $this->_apiUrl);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        
+        curl_setopt($ch, CURLOPT_HTTPHEADER,['Content-Type:application/json']);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $request);
+        
+        $result = curl_exec($ch);
+        
+        if($result !== false) {
+            return json_decode($result, true);
+        }
+        else {
+            throw new Exception(curl_error($ch));
+        }
+    }
+    
+    public function auth() {
+        $request = [
+            'jsonrpc' => '2.0',
+            'method' => 'user.login',
+            'params' => [
+                'user' => $this->_user,
+                'password' => $this->_password
+            ],
+            'id' => 0
+        ];
+        
+        $result = $this->_request($request);
+        
+        $this->_auth = $result['result'];
+    }
+        
+    
+    public function getHosts($hostIds = []) {
+        $request = [
+            'jsonrpc' => '2.0',
+            'method' => 'host.get',
+            'params' => [
+                'output' => 'extend',
+                'hostids' => $hostIds // extend if necessary
+            ],
+            'auth' => $this->_auth,
+            'id' => 0
+        ];
+        
+        $result = $this->_request($request);
+        
+        return $result;
+    }
+    
+    public function getApplicationsByHostId($hostIds) {
+        $request = [
+            'jsonrpc' => '2.0',
+            'method' => 'application.get',
+            'params' => [
+                'hostids' => $hostIds,
+                'output' => 'extend'
+            ],
+            'auth' => $this->_auth,
+            'id' => 0
+        ];
+        
+        $result = $this->_request($request);
+        var_dump($result); exit;
+        
+        return $result;
+    }
+    
+    public function getItemsByHost() {
+        
+    }
+    
+    public function createItems() {
+        
     }
 
 }

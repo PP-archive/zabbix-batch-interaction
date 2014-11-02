d03b5094413cfcc8434fe354004395dca42ae772
Pavel Polyakov <pavel@kredito.de>
2014-11-02 20:57:06 +0200

    sync

--- index.php
+++ index.php
@@ -7,6 +7,14 @@ $f3 = require('vendor/bcosca/fatfree/lib/base.php');
 $f3->config('application.cfg');
 $config = $f3->get('config');
 
+// trying to read the .config file
+if (file_exists('.config')) {
+    $_config = json_decode(file_get_contents('.config'), true);
+
+    $config = array_merge($config, $_config);
+}
+
+
 $f3->route('GET /', function($f3) use ($config) {
 //        $zabbixClient = new Zabbix\Client($config['apiUrl'], $config['user'], $config['password']);
 //        
@@ -15,13 +23,13 @@ $f3->route('GET /', function($f3) use ($config) {
 //        $zabbixClient->getApplicationsByHostId([10207]);
     //$f3->set('var', $zabbixClient->hello());
 
-    if(is_file('.config')) {
+    if (is_file('.config')) {
         $_config = json_decode(file_get_contents('.config'), true);
     }
-    
-    $f3->set('api_url', isset($_config['api_url'])?$_config['api_url']:null);
-    $f3->set('user', isset($_config['user'])?$_config['user']:null);
-    $f3->set('password', isset($_config['password'])?$_config['password']:null);
+
+    $f3->set('api_url', isset($_config['api_url']) ? $_config['api_url'] : null);
+    $f3->set('user', isset($_config['user']) ? $_config['user'] : null);
+    $f3->set('password', isset($_config['password']) ? $_config['password'] : null);
 
     $template = new Template();
     echo $template->render('templates/index.htm');
@@ -61,10 +69,10 @@ $f3->route('POST /config', function($f3) use ($config) {
     $_config = ['api_url' => $f3->get('POST.api_url'),
         'user' => $f3->get('POST.user'),
         'password' => $f3->get('POST.password')];
-    
+
     file_put_contents("./.config", json_encode($_config));
-    
-    Header("Location: ".$config['home']);
+
+    Header("Location: " . $config['home']);
 }
 );
 
